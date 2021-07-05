1fc9b9228a7f2f65bc749d1e65a182148147f3a2
danchitnis <danchitnis@users.noreply.github.com>
2021-07-05 17:28:45 +0100

    Update ubuntu-run.sh

--- build/ubuntu-run.sh
+++ build/ubuntu-run.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
+echo Entryponit script is Running...
+echo
 
-# Add users
-# bash /usr/bin/create-users.sh
+users=$(($#/3))
+mod=$(($# % 3))
 
-addgroup --gid 999 foo
-useradd -m -u 999 -s /bin/bash -g foo foo
-echo foo:bar | /usr/sbin/chpasswd
-usermod -aG sudo foo
+#echo "users is $users"
+#echo "mod is $mod"
+
+if [[ $# -eq 0 ]]; then 
+    echo "No input parameters. exiting..."
+    echo "there should be 3 input parameters per user"
+    exit
+fi
+
+if [[ $mod -ne 0 ]]; then 
+    echo "incorrect input. exiting..."
+    echo "there should be 3 input parameters per user"
+    exit
+fi
+echo "You entered $users users"
 
 
+while [ $# -ne 0 ]; do
 
+    addgroup $1
+    #echo "username is $1"
+    useradd -m -s /bin/bash -g $1 $1
+    wait
+    #getent passwd | grep foo
+    echo $1:$2 | chpasswd 
+    wait
+    #echo "sudo is $3"
+    if [[ $3 == "yes" ]]; then
+        usermod -aG sudo $1
+    fi
+    wait
+    echo "user '$1' is added"
+
+    # Shift all the parameters down by three
+    shift 3
+done
 
 
 
