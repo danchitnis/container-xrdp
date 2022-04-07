aaecb5929274da46863d1b5c02d3ebe967f5e52f
timwa0669 <timwa0669@icloud.com>
2022-04-07 15:31:05 +0800

    Removed useradd scripts

--- build/run.sh
+++ build/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 
 start_xrdp_services() {
     # Preventing xrdp startup failure
@@ -18,50 +18,7 @@ stop_xrdp_services() {
     exit 0
 }
 
-echo Entryponit script is Running...
-echo
-
-users=$(($#/3))
-mod=$(($# % 3))
-
-echo "users is $users"
-echo "mod is $mod"
-
-if [[ $# -eq 0 ]]; then
-    echo "No input parameters. exiting..."
-    echo "there should be 3 input parameters per user"
-    exit
-fi
-
-if [[ $mod -ne 0 ]]; then
-    echo "incorrect input. exiting..."
-    echo "there should be 3 input parameters per user"
-    exit
-fi
-echo "You entered $users users"
-
-while [ $# -ne 0 ]; do
-
-    #echo "username is $1"
-    useradd $1
-    wait
-    #getent passwd | grep foo
-    echo $1:$2 | chpasswd
-    wait
-    #echo "sudo is $3"
-    if [[ $3 == "yes" ]]; then
-        usermod -aG wheel $1
-    fi
-    wait
-    echo "user '$1' is added"
-
-    # Shift all the parameters down by three
-    shift 3
-done
-
-echo -e "This script is ended\n"
-
-echo -e "starting xrdp services...\n"
+echo -e "Starting xrdp services...\n"
 
 trap "stop_xrdp_services" SIGKILL SIGTERM SIGHUP SIGINT EXIT
 start_xrdp_services
